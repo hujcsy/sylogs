52dd9bf2cfe94638151f279883052145a470aabc
hujcsy <126960850+hujcsy@users.noreply.github.com>
2026-07-02 02:47:17 -0400

    Update test.js

--- functions/api/test.js
+++ functions/api/test.js
@@ -1,3 +1,10 @@
 export async function onRequest(context) {
-  return new Response("Hello from Cloudflare Functions!");
+  try {
+    await context.env.DB.prepare("INSERT INTO logs (message) VALUES (?)")
+            .bind("有人访问了网站！")
+            .run();
+    return new Response("數據庫寫入成功！", { status: 200 });
+  } catch (err) {
+    return new Response("錯誤: " + err.message, { status: 500 });
+  }
 }
